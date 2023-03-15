6d2cfab31f86e5863b789b09589c60ec5c330b7f
Chris <34217923+Chris-8@users.noreply.github.com>
2023-03-14 21:20:08 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,6 +5,7 @@ class minecraft {
   file {'/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar',
+    before => Service['minecraft'],
   }
   yumrepo { 'packages-microsoft-com-prod':
     baseurl   => 'https://packages.microsoft.com/centos/7/prod',
@@ -15,7 +16,7 @@ class minecraft {
     sslverify => 1,
   }
   package { 'msopenjdk-17':
-    #require => yumrepo['packages-microsoft-com-prod'],
+    require => Yumrepo['packages-microsoft-com-prod'],
     ensure => present,
   }
   file { '/opt/minecraft/eula.txt':
@@ -28,6 +29,7 @@ class minecraft {
   }
   service { 'minecraft':
     ensure  => running,
-    enable => true
+    enable  => true
+    require => [Package['msopenjdk-17'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']]
   }
 }
