48dd2553c201fe8db34a6320d0bbdb06270d84cb
Chris <34217923+Chris-8@users.noreply.github.com>
2023-03-14 21:28:36 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,12 +1,6 @@
 class minecraft {
-  file { '/opt/minecraft':
-    ensure => directory,
-  }
-  file {'/opt/minecraft/server.jar':
-    ensure => file,
-    source => 'https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar',
-    before => Service['minecraft'],
-  }
+  $mincraft_jar_url = 'https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar'
+  $install_dir = '/opt/minecraft'
   yumrepo { 'packages-microsoft-com-prod':
     baseurl   => 'https://packages.microsoft.com/centos/7/prod',
     name     => 'packages-microsoft-com-prod',
@@ -19,7 +13,15 @@ class minecraft {
     require => Yumrepo['packages-microsoft-com-prod'],
     ensure => present,
   }
-  file { '/opt/minecraft/eula.txt':
+  file { $install_dir:
+    ensure => directory,
+  }
+  file {"${$install_dir}/server.jar":
+    ensure => file,
+    source => $mincraft_jar_url,
+    before => Service['minecraft'],
+  }
+  file { "${$install_dir}/eula.txt":
     ensure  => file,
     content => 'eula=true',
   }
@@ -30,6 +32,6 @@ class minecraft {
   service { 'minecraft':
     ensure  => running,
     enable  => true,
-    require => [Package['msopenjdk-17'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']],
+    require => [Package['msopenjdk-17'],File["${$install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
 }
